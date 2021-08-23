60e202893e7c6f1039493a44bf390149bb86edf4
NoMeNoMeMe <mateusz.witkowski0000@gmail.com>
2021-08-23 23:52:37 +0200

    Added game rooms
    Added package react-router-dom

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React, { lazy, Suspense } from "react";
+import { Route, Switch } from "react-router";
+
+const Home = lazy(() => import('../pages/home'));
+const Room = lazy(() => import('../pages/room'));
+
+const Routes = () => (
+    <Switch>
+        <Suspense fallback={<h1>Loading page...</h1>}>
+            <Route path='/room/:id' component={Room}></Route>
+            <Route exact path='/' component={Home}></Route>
+        </Suspense>
+    </Switch>
+)
+
+export default Routes
